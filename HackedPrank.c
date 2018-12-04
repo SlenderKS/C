bb8c66cafded9c4f475ab0f259eef3cc859d095c
LucasXD01 <lucaz60@yahoo.com.br>
2018-12-04 04:52:27 -0200

    hacked prank

---
+++ HackedPrank.c
@@ -0,0 +1,30 @@
+#include<stdio.h>																//Importando "bibliotecas" para escrever no seu terminal.
+#include<unistd.h>																//Importando "bibliotecas" para fazer o tempo de espera.
+			
+ /*Made by Slender (Lucas E.)  */												
+
+int main(){                //Inicio do programa.     
+	int i;				   // declarando a variavel para a parte de repetição.										
+	for(i=0;i<3;i++){      // tudo dentro desse colchete  -> {   e o q fecha na linha 24 vai se repetir 3 vezes   
+printf("\n");																						// pula inha			
+printf("\n");																					    // pula inha		
+printf("\n");																						// pula inha		
+printf("O))     O))      O)           O))   O))   O))  O))))))))O)))))    \n");           // manda linha de código na sua tela
+printf("O))     O))     O) ))      O))   O))O))  O))   O))      O))   O)) \n");           // manda linha de código na sua tela
+printf("O))     O))    O)  O))    O))       O)) O))    O))      O))    O))\n");            // manda linha de código na sua tela
+printf("O)))))) O))   O))   O))   O))       O) O)      O))))))  O))    O))\n");            // manda linha de código na sua tela
+printf("O))     O))  O)))))) O))  O))       O))  O))   O))      O))    O))\n");            // manda linha de código na sua tela
+printf("O))     O)) O))       O))  O))   O))O))   O))  O))      O))   O)) \n");             // manda linha de código na sua tela
+printf("O))     O))O))         O))   O))))  O))     O))O))))))))O)))))    \n");             // manda linha de código na sua tela
+printf("\n");	                                                                                   // pula linha
+printf("\n"); 																			          // pula inha
+printf("\n");																			          // pula inha
+
+ sleep(1);   // comando para dar uma pausa de 1 segundo
+				}
+				
+	 sleep(3);																	    // parada dramática uhasuhsa
+	 printf("Calma, é só uma brincadeira uhuassahusauhsa   (msg automática)\n");    // explicação dizendo q não digitei aquilo agr
+	 sleep(3);                                                                       // parada para respeirar uhsauhsauh
+	 printf("Tá aqui a foto do codigo fonte, para você  ver que é inofencivo   http://prntscr.com/lqemag  ");    //  provando que é brincadeira :D
+}
